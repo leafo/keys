ed11f3c481ab4fa0404c07b78bceb36db7571fc6
leaf corcoran <leafot@gmail.com>
2017-10-20 10:25:49 -0700

    update backup to create schema separately

--- backup.sh
+++ backup.sh
@@ -5,8 +5,10 @@ PASSWORD=${KEYS_PASSWORD:?"Missing password"}
 DATABASE=${KEYS_DATABASE:-"leaf_selfspy"}
 TABLE=${KEYS_TABLE:-"keys2"}
 
+ssh leaf@leafo.net "mysqldump -u $USER -p$PASSWORD $DATABASE $TABLE --compatible=postgresql --no-data" > backup_schema.sql
+
 for machine_id in $(seq 1 4); do
 	echo "Machine ID $machine_id"
-	ssh leaf@leafo.net "mysqldump -u $USER -p$PASSWORD $DATABASE $TABLE --where=\"machine_id=$machine_id\" --compatible=postgresql | gzip" | pv >  machine$machine_id.sql.gz
+	ssh leaf@leafo.net "mysqldump -u $USER -p$PASSWORD $DATABASE $TABLE --where=\"machine_id=$machine_id\" --compatible=postgresql --no-create-info | gzip" | pv >  machine$machine_id.sql.gz
 done
 
